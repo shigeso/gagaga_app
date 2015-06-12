c0c26101403dd34e1357c7f21a417a19e723210a
sakiyama shigen <shtaarddy918@gmail.com>
2015-06-12 11:26:08 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
   describe "Home page" do
-
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
@@ -11,7 +12,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -24,7 +25,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -38,6 +39,19 @@ describe "StaticPages" do
 
   it "should have the right title" do
     visit '/static_pages/home'
-    expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    expect(page).to have_title("#{base_title} | About Us")
+  end
+
+  describe "Contact page" do
+  	# expect(page).toの前の実行をここでまとめた２行短縮
+  	before { visit '/static_pages/contact' }
+
+    it "should have the content 'Contact'" do
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      expect(page).to have_title("#{base_title} | Contact")
+    end
   end
 end
